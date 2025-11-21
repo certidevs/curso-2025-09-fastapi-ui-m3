6f5e82d703d173cde909649d574be6034a4abc20
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-11-21 14:01:35 +0100

    listado de canciones

--- cancioncitas/app/routers/web/__init__.py
+++ cancioncitas/app/routers/web/__init__.py
@@ -4,8 +4,11 @@ Contienen los endpoints que renderizan HTMLs
 """
 
 from app.routers.web import home
+from app.routers.web import songs
+
 from fastapi import APIRouter
 
 router = APIRouter()
 
 router.include_router(home.router)
+router.include_router(songs.router)
