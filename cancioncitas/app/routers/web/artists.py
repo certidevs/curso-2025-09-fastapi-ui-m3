8e19ab8dd0b818b10c96d5749380595760729e42
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-11-25 12:03:58 +0100

    método listar artistas

--- cancioncitas/app/routers/web/artists.py
+++ cancioncitas/app/routers/web/artists.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from app.database import get_db
+from app.models import Artist
 
 # configuración de Jinja2Templates
 templates = Jinja2Templates(directory="app/templates")
 
 # router para rutas web
 router = APIRouter(prefix="/artists", tags=["web"])
+
+@router.get("", response_class=HTMLResponse)
+def list_artists(request: Request, db: Session = Depends(get_db)):
+    artists = db.execute(select(Artist)).scalars().all()
+    
+    return templates.TemplateResponse(
+        "artists/list.html",
+        {"request": request, "artists": artists}
+    )
