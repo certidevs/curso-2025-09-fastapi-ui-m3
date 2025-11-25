6c0a620d4b9febb4b55dd80110fc4ed1333a13ed
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-11-25 13:36:43 +0100

    metodo get detalle cancion

--- cancioncitas/app/routers/web/songs.py
+++ cancioncitas/app/routers/web/songs.py
@@ -1,5 +1,5 @@
 from fastapi.templating import Jinja2Templates
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from sqlalchemy import select
@@ -18,4 +18,17 @@ def list_songs(request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse(
         "songs/list.html",
         {"request": request, "songs": songs}
+    )
+
+# detalle canción (http://localhost:8000/songs/5)
+@router.get("/{song_id}", response_class=HTMLResponse)
+def song_detail(request: Request, song_id: int, db: Session = Depends(get_db)):
+    song = db.execute(select(Song).where(Song.id == song_id)).scalar_one_or_none()
+    
+    if song is None:
+        raise HTTPException(status_code=404, detail="404 - Canción no encontrada")
+    
+    return templates.TemplateResponse(
+        "songs/detail.html",
+        {"request": request, "song": song}
     )
