b9c247e16dd4555b27e956ea936a5936374df1df
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-12-09 11:31:35 +0100

    añadir Concert y ConcertStatus al init de models

--- cancioncitas/app/models/__init__.py
+++ cancioncitas/app/models/__init__.py
@@ -4,5 +4,6 @@ Modelos de base de datos (SQLAlchemy)
 
 from app.models.song import Song
 from app.models.artist import Artist
+from app.models.concert import Concert, ConcertStatus
 
-__all__ = ["Song", "Artist"]
+__all__ = ["Song", "Artist", "Concert", "ConcertStatus"]
