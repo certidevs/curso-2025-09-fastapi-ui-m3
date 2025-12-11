7fe815f869105fbdafd7ce52dac7be40352a96c0
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-12-11 10:06:43 +0100

    añadir schemas al init

--- cancioncitas/app/schemas/__init__.py
+++ cancioncitas/app/schemas/__init__.py
@@ -4,5 +4,6 @@ Esquemas Pydantic para validación de datos
 
 from app.schemas.song import SongResponse, SongCreate, SongUpdate, SongPatch
 from app.schemas.artist import ArtistResponse
+from app.schemas.concert import ConcertResponse, ConcertCreate, ConcertPatch
 
-__all__ = ["SongResponse", "SongCreate", "SongUpdate", "SongPatch", "ArtistResponse"]
+__all__ = ["SongResponse", "SongCreate", "SongUpdate", "SongPatch", "ArtistResponse", "ConcertResponse", "ConcertCreate", "ConcertPatch"]
