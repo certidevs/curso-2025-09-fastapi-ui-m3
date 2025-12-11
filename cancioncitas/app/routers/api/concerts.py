75e2e7e8ed30bb822b6136f1064fb025e921a5de
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-12-11 10:25:55 +0100

    método GET find_all

--- cancioncitas/app/routers/api/concerts.py
+++ cancioncitas/app/routers/api/concerts.py
@@ -1,6 +1,16 @@
 from fastapi import Depends, HTTPException, status, APIRouter
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
+
 from app.database import get_db
+from app.models import Concert
+from app.schemas import ConcertResponse
 
 router = APIRouter(prefix="/api/concerts", tags=["concerts"])
+
+# obtener todos los conciertos
+@router.get("", response_model=list[ConcertResponse])
+def find_all(db: Session = Depends(get_db)):
+    return db.execute(
+        select(Concert).options(joinedload(Concert.artist))
+    ).scalars().unique().all()
