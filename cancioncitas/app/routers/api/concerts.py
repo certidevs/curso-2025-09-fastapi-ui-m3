ff162b6f2beaf6d29a366824e0c85328a3e99cc3
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-12-11 11:25:38 +0100

    método POST create concert

--- cancioncitas/app/routers/api/concerts.py
+++ cancioncitas/app/routers/api/concerts.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session, joinedload
 
 from app.database import get_db
 from app.models import Concert
-from app.schemas import ConcertResponse
+from app.schemas import ConcertResponse, ConcertCreate
 
 router = APIRouter(prefix="/api/concerts", tags=["concerts"])
 
@@ -31,3 +31,30 @@ def find_by_id(id: int, db: Session = Depends(get_db)):
         )
     
     return concert
+
+# crear un nuevo concierto
+@router.post("", response_model=ConcertResponse, status_code=status.HTTP_201_CREATED)
+def create(concert_dto: ConcertCreate, db: Session = Depends(get_db)):
+    
+    concert = Concert(
+        name=concert_dto.name,
+        price=concert_dto.price,
+        capacity=concert_dto.capacity,
+        status=concert_dto.status,
+        is_sold_out=concert_dto.is_sold_out,
+        date_time=concert_dto.date_time,
+        img_url=concert_dto.img_url,
+        artist_id=concert_dto.artist_id
+    )
+    
+    db.add(concert)
+    db.commit()
+    db.refresh(concert)
+    
+    concert_with_artist = db.execute(
+        select(Concert)
+        .where(Concert.id == concert.id)
+        .options(joinedload(Concert.artist))
+    ).scalar_one()
+    
+    return concert_with_artist
